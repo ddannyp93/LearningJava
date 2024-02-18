bf1a951f584100b9fbd74477212ebf0d1ac2f020
ddannyp93 <stud-dannyp@ruc.dk>
2024-02-18 13:30:18 +0100

    Start 2.0

---
+++ src/Shapes.java
@@ -0,0 +1,13 @@
+public abstract class Shapes {
+    int x;
+    int y;
+
+    public Shapes(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract double area();
+
+
+}
