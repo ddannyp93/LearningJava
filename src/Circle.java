b010142d4a7a16cfe5fc731182d278c1dbd7c86c
ddannyp93 <stud-dannyp@ruc.dk>
2024-02-18 13:45:58 +0100

    Circle area added

--- src/Circle.java
+++ src/Circle.java
@@ -1,10 +1,14 @@
 public class Circle extends Shapes {
+    private int radius;
+
     public Circle(int x, int y, int radius) {
+
         super(x, y);
+        this.radius = radius;
     }
 
     @Override
     public double area() {
-        return 0;
+        return 3.14*(radius*radius);
     }
 }
