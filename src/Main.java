95e6cbd6d40259eecaede82a1900401c3aa07ce5
ddannyp93 <stud-dannyp@ruc.dk>
2024-02-18 13:39:00 +0100

    Created my Rectangle

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
 
+        Rectangle my_rectangle = new Rectangle(2,4,4,10);
+        System.out.println("My rectangle is this big = " + my_rectangle.area() + " cm2" );
+    }
 }
